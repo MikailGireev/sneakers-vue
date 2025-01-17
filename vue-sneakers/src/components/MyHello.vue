90faa2f2fb62ebdc1b31b64be5ca117e6c3c3821
Mikail <mikaildevelop@gmail.com>
2025-01-17 06:14:42 +0300

    Tallwind install

---
+++ vue-sneakers/src/components/MyHello.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+
+let count = ref(0);
+
+function increment() {
+  count.value++;
+}
+</script>
+
+<template>
+  <div class="mb-10 text-slate-500">
+    <h1>{{ count }}</h1>
+    <button @click="increment">+</button>
+  </div>
+</template>
